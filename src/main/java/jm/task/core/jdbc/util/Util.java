81d69b73feb3e0043a8a7228e698fa331a7a3a2d
Ildar Khuzin <ildar.khuzin75@mail.ru>
2024-09-27 17:51:33 +0300

    сделал задачку

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/myDB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin";
+
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static Connection getConnection() {
+        return connection;
+    }
 }
